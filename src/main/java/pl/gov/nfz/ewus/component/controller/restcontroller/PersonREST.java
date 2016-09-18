236f5b64e5b6d14bc4db948eb1f0646a1117538c
Tomasz Jasinski <tomjas@gmail.com>
2016-09-18 19:38:20 +0200

    Added rest controller for update and register people

---
+++ src/main/java/pl/gov/nfz/ewus/component/controller/restcontroller/PersonREST.java
@@ -0,0 +1,43 @@
+package pl.gov.nfz.ewus.component.controller.restcontroller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import pl.gov.nfz.ewus.component.service.PersonService;
+import pl.gov.nfz.ewus.model.Person;
+
+@RestController
+public class PersonREST {
+
+	@Autowired
+	PersonService personService;
+
+	@RequestMapping(value = "/people", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Person>> getRegisteredPeople() {
+		List<Person> registeredPeople = personService.getAllRegisteredPeople();
+		return new ResponseEntity<List<Person>>(registeredPeople, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/register", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Person> registerPerson(@RequestBody final Person person) {
+		Person registeredPerson = personService.register(person);
+		return new ResponseEntity<Person>(registeredPerson, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/update", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Person> updatePerson(@RequestBody final Person person) {
+		Person updatedPerson = personService.update(person);
+		return new ResponseEntity<Person>(updatedPerson, HttpStatus.OK);
+	}
+
+}
--- src/main/java/pl/gov/nfz/ewus/component/controller/restcontroller/PeselREST.java
+++ src/main/java/pl/gov/nfz/ewus/component/controller/restcontroller/PeselREST.java
@@ -1,25 +1,20 @@
-package pl.gov.nfz.ewus.restcontroller;
-
-import java.util.List;
+package pl.gov.nfz.ewus.component.controller.restcontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import pl.gov.nfz.ewus.PeselValidator;
-import pl.gov.nfz.ewus.model.Person;
-import pl.gov.nfz.ewus.service.PersonService;
+import pl.gov.nfz.ewus.component.service.PersonService;
+import pl.gov.nfz.ewus.model.InsuranceStatus;
 
 @RestController
 public class PeselREST {
 
-	@Autowired
-	private PeselValidator peselValidator;
-	
 	@Autowired
 	private PersonService personService;
 
@@ -27,16 +22,11 @@ public class PeselREST {
 		// TODO Auto-generated constructor stub
 	}
 
-	@RequestMapping(value = "/isInsured", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Person> isInsured() {
-		Person person = new Person();
-		person.setPesel(76010401692l);
-		return new ResponseEntity<Person>(person, HttpStatus.OK);
-	}
-	
-	@RequestMapping(value = "/people" ,method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<Person>> getRegisteredPeople(){
-		
+	@RequestMapping(value = "/isInsured", method = RequestMethod.POST, consumes = MediaType.TEXT_PLAIN_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<InsuranceStatus> isInsured(@RequestBody final String pesel) {
+		InsuranceStatus status = personService.getInsuranceStatus(pesel);
+		return new ResponseEntity<InsuranceStatus>(status, HttpStatus.OK);
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/checkConnection")
